9b024ece1dac68e4c822652c50685096f2dcf3d5
Dikshit-Sharma <dikshit.sharma2580@gmail.com>
2025-12-14 13:23:44 +0530

    Upload Docs SupaBase

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -11,7 +11,7 @@ export function createApp() {
       origin: true, // reflect request origin
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization']
+      allowedHeaders: ['Content-Type', 'Authorization', 'x-user-token']
     })
   );
 
